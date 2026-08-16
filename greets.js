d700b109f289593812e9dd7c4a376bfe2cea41e1
idannadler <idan70000@gmail.com>
2026-08-16 22:46:35 +0300

    new greet jenkins 14

--- greets.js
+++ greets.js
@@ -20,6 +20,7 @@ const greets = [
   "do not wait to strike till the iron is hot; but make it hot by striking.",
   "great minds discuss ideas; average minds discuss events; small minds discuss people.",
   "do not go where the path may lead, go instead where there is no path and leave a trail.",
+  "failure will never overtake me if my determination to succeed is strong enough.",
   "",
 ];
 
